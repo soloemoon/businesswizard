b21faf3d78a24276a65b3f9118310df1c33248ac
Solomon Moon <57546826+soloemoon@users.noreply.github.com>
2024-07-05 04:41:24 -0500

    Update group_concat.py

--- businesswizard/functions/group_concat.py
+++ businesswizard/functions/group_concat.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import pandas_flavor as pf
+from typing import List
 
 
 @pf.register_dataframe_method
 def group_concat(
     df: pd.DataFrame,
-    group_column: str | list,
-    concat_column: str | list,
+    group_column: str | List[str],
+    concat_column: str | List[str],
     delimiter: str = '|'
 ) -> pd.DataFrame:
     '''
@@ -17,9 +18,9 @@ def group_concat(
     ----------
     df: pd.DataFrame
         Dataframe
-    group_column: str | list
+    group_column: str | List[str]
         Name of column(s) to group by. Multiple names must be in a list.
-    concat_column: str | list
+    concat_column: str | List[str]
         Name of column(s) to collapse. Multiple names must be in a list
     delimiter: str
         Type of delimiter to use for separation of concat column values   
@@ -28,6 +29,13 @@ def group_concat(
     result: pd.DataFrame
         Summarised dataframe with one row (grouping) and multiple values
     '''
-    
-    result = df.groupby(group_column)[concat_column].agg(lambda x: f'{delimiter} '.join(x))
+
+    col_dict = {}
+    for col in df:
+        if (col in concat_column):
+            col_dict[col] = f' {delimiter} '.join
+        else:
+            col_dict[col] = 'first'
+               
+    result = df.groupby(group_column, as_index=False).agg(col_dict)
     return result
